e8508c8d2e07f7c50ad190299bdf0173f5f4441c
meehl <12648828+meehl@users.noreply.github.com>
2023-11-29 22:31:27 +0100

    Remove apply button from SrtSettingsInput and bind instead

--- src/lib/SrtSettingsInput.svelte
+++ src/lib/SrtSettingsInput.svelte
@@ -1,40 +1,17 @@
 <script lang="ts">
-  import { useForm, Hint, validators, required } from 'svelte-use-form'
   import { srtSettings } from '../store'
-
-  const requiredMessage = 'This field is required'
-  const form = useForm({
-    streamUrl: { initial: $srtSettings.streamUrl },
-    streamId: { initial: $srtSettings.streamId },
-    pollingInterval: { initial: $srtSettings.pollingInterval.toString() },
-    remember: {},
-  })
-
-  const formSubmit = () => {
-    if ($form.valid) {
-      const streamUrl = $form.streamUrl.value
-      const streamId = $form.streamId.value
-      const pollingInterval = Number($form.pollingInterval.value)
-      srtSettings.set({ streamUrl, streamId, pollingInterval })
-    }
-  }
 </script>
 
-<form use:form on:submit|preventDefault={formSubmit}>
+<div>
   <label for="streamUrl">Stream URL</label>
-  <input type="text" name="streamUrl" use:validators={[required]} />
-  <Hint for="streamUrl" on="required">{requiredMessage}</Hint>
+  <input type="text" name="streamUrl" bind:value={$srtSettings.streamUrl} />
 
   <label for="streamId">Stream ID</label>
-  <input type="text" name="streamId" use:validators={[required]} />
-  <Hint for="streamId" on="required">{requiredMessage}</Hint>
+  <input type="text" name="streamId" bind:value={$srtSettings.streamId} />
 
   <label for="pollingInterval">Polling Interval (ms)</label>
-  <input type="number" name="pollingInterval" use:validators={[required]} />
-  <Hint for="pollingInterval" on="required">{requiredMessage}</Hint>
-
-  <button disabled={!$form.valid}>Apply</button>
-</form>
+  <input type="number" name="pollingInterval" bind:value={$srtSettings.pollingInterval} />
+</div>
 
 <style>
 </style>
